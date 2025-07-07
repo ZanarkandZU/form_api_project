1cf4f9e4591057f3698888b1f53d091b151dc404
ZanarkandZU <muhammadamrullahazzaqi@gmail.com>
2025-07-07 12:00:32 +0700

    connect database url

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -9,7 +9,12 @@ const testConfig = {
   database: process.env.PGDATABASE_TEST,
 };
 
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+};
+
 const pool =
-  process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
+  process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool(config);
 
 module.exports = pool;
